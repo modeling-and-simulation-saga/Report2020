c12e95f66206f1f0f9ddb0d85c69807e804bfb18
tada195908 <48470775+tada195908@users.noreply.github.com>
2021-01-07 16:46:47 +0900

    Update CentralLimitingTheorem.java

--- src/observe/CentralLimitingTheorem.java
+++ src/observe/CentralLimitingTheorem.java
@@ -1,13 +1,7 @@
 package observe;
 
 import histogram.Histogram;
-import java.awt.geom.Point2D;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.util.List;
 import java.util.Random;
-import method.Uniform;
-import myLib.utils.FileIO;
 
 /**
  *
@@ -40,26 +34,6 @@ public class CentralLimitingTheorem {
         
         
         return sum;
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
-        Uniform myRandom = new Uniform(0, 1);
-        int sampleSize = 100;
-        int numSamples = 100000;
-        Histogram histogram = new Histogram(30, 70, 100);
-        CentralLimitingTheorem sys = new CentralLimitingTheorem(myRandom, sampleSize);
-        sys.generateHistogram(histogram, numSamples);
-        List<Point2D.Double> plist = histogram.calculateFrequency();
-
-        String filename = "histogram.txt";
-        try ( BufferedWriter out = FileIO.openWriter(filename)) {
-            for (Point2D.Double p : plist) {
-                FileIO.writeSSV(out, p.x, p.y);
-            }
-        }
     }
 
 }
